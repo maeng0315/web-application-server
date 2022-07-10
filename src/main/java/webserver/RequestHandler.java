d18832a1711c60dd9365aea33621127ffe9dc87a
DESKTOP-COO13VP\dakma <dakma@naver.com>
2022-07-10 17:37:12 +0900

    4.2.1.6 요구사항 6 - 사용자 목록 출력 (136p) HTTP는 기본적으로 무상태 프로토콜이기에 각 요청 간에 상태 정보를 공유하기 위한 유일한 방법으로 쿠키를 사용한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Map;
 
 import db.DataBase;
@@ -38,6 +39,7 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
             int contentLength = 0;
+            boolean logined = false;
 
             while (!"".equals(line)) {
                 line = br.readLine();
@@ -47,9 +49,14 @@ public class RequestHandler extends Thread {
                     contentLength = getContentLength(line);
                 }
 
+                if (line.contains("Cookie")) {
+                    logined = isLogin(line);
+                }
+
             }
 
             String url = tokens[1];
+
             if (url.startsWith("/user/create")) {
                 String requestBody = IOUtils.readData(br, contentLength);
                 log.debug("requestBody : {}", requestBody);
@@ -77,12 +84,45 @@ public class RequestHandler extends Thread {
                 } else {
                     responseResource(out, "/user/login_failed.html");
                 }
+            } else if ("/user/list".equals(url)) {
+
+                if (!logined) {
+                    responseResource(out, "/user/login.html");
+                    return;
+                }
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             } else {
                 responseResource(out, url);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void responseResource(OutputStream out, String url) throws IOException {
