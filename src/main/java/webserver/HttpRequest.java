7f0ed45f5c11d4e0e3c6287545167f65fa1aaa63
DESKTOP-COO13VP\dakma <dakma@naver.com>
2022-07-16 19:53:32 +0900

    5.1.2.1 요청 데이터를 처리하는 로직을 별도의 클래스로 분리한다(HttpRequest) (147p)

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -11,40 +11,57 @@ import java.util.Map;
 
 public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
-    private final InputStream in;
-    private String[] tokens;
     private String method;
     private String path;
-    private Map<String, String> headerMap = new HashMap<>();
-    private BufferedReader br;
+    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> params = new HashMap<>();
 
-    public HttpRequest(InputStream in) throws IOException {
-        this.in = in;
-        String line = getRequestLine(in);
-        setRequsetHeader(line);
-    }
-
-    private String getRequestLine(InputStream in) throws IOException {
-        br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        String line = br.readLine();
-        if (line == null) {
-            return "";
-        }
-        log.debug("request line : {}", line);
-        this.tokens = line.split(" ");
-        this.method = this.tokens[0];
-        this.path = this.tokens[1].split("[?]")[0];
-        return line;
-    }
-
-    private void setRequsetHeader(String line) throws IOException {
-        while (line != null) {
-            line = br.readLine();
-            HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
-            if (pair == null) {
+    public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if (line == null) {
                 return;
             }
-            headerMap.put(pair.getKey(), pair.getValue());
+
+            processRequestLine(line);
+
+            line = br.readLine();
+            while (!"".equals(line)) {
+                if (line == null) {
+                    return;
+                }
+                log.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if ("POST".equals(method)) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        } catch (IOException io) {
+            log.error(io.getMessage());
+        }
+    }
+
+    private void processRequestLine(String requestLine) throws IOException {
+        log.debug("request line : {}", requestLine);
+        String[] tokens = requestLine.split(" ");
+        method = tokens[0];
+
+        if ("POST".equals(method)) {
+            path = tokens[1];
+            return;
+        }
+
+        int index = tokens[1].indexOf("?");
+        if (index == -1) {
+            path = tokens[1];
+        } else {
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index+1));
         }
     }
 
@@ -56,21 +73,11 @@ public class HttpRequest {
         return this.path;
     }
 
-    public String getHeader(String key) {
-        return headerMap.get(key);
+    public String getHeader(String name) {
+        return headers.get(name);
     }
 
-    public String getParameter(String key) throws IOException {
-        String queryString = "";
-        if ("POST".equals(this.method)) {
-            queryString = IOUtils.readData(br, Integer.parseInt(getHeader("Content-Length")));
-            log.debug("Requestbody : {}", queryString);
-        } else {
-            int queryStringStartIndex = tokens[1].indexOf("?") + 1;
-            queryString = tokens[1].substring(queryStringStartIndex);
-        }
-        Map<String, String> parseQueryString = HttpRequestUtils.parseQueryString(queryString);
-        log.debug("parseQueryString : {}", parseQueryString);
-        return parseQueryString.get(key);
+    public String getParameter(String name) {
+        return params.get(name);
     }
 }
